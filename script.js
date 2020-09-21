5d639b19e5e7206297becd4c99d0b9891c09b7e3
Yuliia <julia.av.ferreira@gmail.com>
2020-09-21 21:00:33 +0200

    completed

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ const games = [
     { id: 28, name: "Terrible game", rating: 1.0 },
     { id: 4, name: "Mediocre game", rating: 3.0 },
 ];
+
+const container = document.querySelector(".games-container");
+
+let html = "";
+
+for(let i = 0; i < games.length; i++) {
+   html += `<h4 class="name">Name: ${games[i].name}</h4>
+                        <p>Rating: ${games[i].rating}</p>`; 
+}
+
+container.innerHTML = html;
+
+const names = document.querySelectorAll(".name");
+
+names.forEach (function(name) {
+    name.addEventListener("click", clickHappened)
+});
+
+function clickHappened(event) {
+    event.target.classList.toggle("played");
+}
